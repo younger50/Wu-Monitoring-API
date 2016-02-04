ffc199a6c7c61e7bb8d1e705cb67004792a6ddbf
younger50 <pt54088@gmail.com>
2016-02-05 00:11:17 +0800

    add create device to db method, add test page built with angularjs

--- nodejs_api/routes/demo.js
+++ nodejs_api/routes/demo.js
@@ -5,47 +5,68 @@ var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var mongodb_url = 'mongodb://localhost:27017/demo';
 
-/* demo home */
-router.get('/', function(req, res, next) {
-  res.send('demo home');
-});
-
-/* GET device list */
-router.get('/get_all_device', function(req, res, next) {
-	res.setHeader('Content-Type', 'application/json');
-	// res.send(JSON.stringify({
-	//   	"status":"get device success",
-	//   	"Devices":
-	//   	[
-	// 	    {
-	// 	      	"WuID":"5566",
-	// 	      	"Name":"light sensor",
-	// 	      	"Location":"bed room"
-	// 	    },
-	// 	    {
-	// 	      	"WuID":"1234",
-	// 	      	"Name":"temperature sensor",
-	// 	      	"Location":"living room"
-	// 	    }
-	//   	]
-	// }));
-	// raw get get method
-	MongoClient.connect( mongodb_url, function(err, db) {
+/* home made mongo db functions */
+//
+var dbfind = function ( target, callback) {
+	MongoClient.connect( mongodb_url, function (err, db) {
 		assert.equal(err, null);
 		console.log("Connected correctly to server.");
-		cursor = db.collection('device').find();
-		list = [];
+		cursor = db.collection('device').find(target);
+		var list = [];
 		cursor.each( function (err, doc) {
 		    assert.equal(err, null);
 		    if (doc != null) {
 		       	console.log(doc);
 		        list.push(doc);
 		    } else {
-		        res.send(list);
+		    	console.log("db find success");
+		        callback(list);
 		        db.close();
 		    }
 	   	});
 	});
+}
+
+var dbinsert = function ( target, callback) {
+	MongoClient.connect( mongodb_url, function (err, db) {
+		assert.equal(err, null);
+		console.log("Connected correctly to server.");
+		cursor = db.collection('device').insertOne( target,
+		function (err, result) {
+		    assert.equal(err, null);
+		    console.log("db insert success");
+		    callback(result);
+		    db.close();
+	   	});
+	});
+}
+
+/* demo home */
+router.get('/', function (req, res, next) {
+  //res.send('demo home');
+  res.render('test_page');
 });
+
+/* Login */
+
+/* POST CreateDevice */
+router.post('/create_device', function (req, res, next) {
+	console.log(req.body);
+	dbinsert( req.body, function (data) {
+		res.send("OK "+JSON.stringify(req.body));
+	});
+});
+
+/* GET GetAllDevice */
+router.get('/get_all_device', function (req, res, next) {
+	dbfind( {}, function (data) {
+		res.setHeader('Content-Type', 'application/json');
+		res.send(data);
+	});
+});
+
+/* POST SendData */
+
+/* GET GetDeviceData */
 
 module.exports = router;
